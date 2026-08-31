dc4d53aa34586d7afd9b66064ed35e315148670c
Tabre Perez <tabre@tabre.com>
2026-08-31 15:06:09 -0500

    fix: permissions on files created by the docker image

--- services/app/init.sh
+++ services/app/init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-foms_root="/root/foms"
+foms_dir="/home/appuser/foms"
 js_dir="src/static/js"
 htmx_file="htmx.min.js"
 htmx_cdn_url="https://cdn.jsdelivr.net/npm/htmx.org@4.0.0/dist/htmx.min.js"
 
-cd $foms_root
+cd $foms_dir
 
 if [[ ! -d "$js_dir" ]]; then
     mkdir -p $js_dir
